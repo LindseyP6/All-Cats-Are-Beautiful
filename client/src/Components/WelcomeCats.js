3b787fe050daf4a2aa37f03e455c3bdb784954c9
Lindsey <lindsey.pregent@gmail.com>
2022-07-18 17:02:45 -0400

    contact updated border pics added

--- client/src/Components/WelcomeCats.js
+++ client/src/Components/WelcomeCats.js
@@ -8,16 +8,15 @@ function WelcomeCats({catsArray}) {
 
   const singleCatImg = catsArray.sort(() => Math.random() - .5).slice(0, 8);
 
-  const toolTip = 
-
   const catImgs = singleCatImg.map((cat) => 
-  // <Card className="cat-welcome-card">
-  //   <Card.Img className="welcome-cat-img" variant="bottom" src={cat.image} />
-  // </Card>
-  <img className="welcome-cat-img" src={cat.image} alt={cat.name}/>
+    <OverlayTrigger
+      placement="top"
+      overlay={<Tooltip>{cat.name}</Tooltip>}
+    >
+      <img className="welcome-cat-img" src={cat.image} alt={cat.name}/>
+    </OverlayTrigger>
   );
 
-  console.log("cats", catImgs)
   return (
     <div className="welcome-cats-main">
         {catImgs}
